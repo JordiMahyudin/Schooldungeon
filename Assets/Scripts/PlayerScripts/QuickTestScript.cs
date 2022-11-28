c2d486f5503cc522329ea1d2eca37c7ac74a43b0
SamBeek <200496@student.glu.nl>
2022-11-28 11:42:36 +0100

    EnemyFlickeringPush
    Added enemy sprite flickering on hit

--- Assets/Scripts/PlayerScripts/QuickTestScript.cs
+++ Assets/Scripts/PlayerScripts/QuickTestScript.cs
@@ -7,13 +7,30 @@ public class QuickTestScript : MonoBehaviour
 
     //THIS IS A QUICK TEST SCRIPT SO THE ENEMY CAN TAKE DAMAGE FOR TESTING (PLEASE DO NOT RELY ON THIS SCRIPT FOR ENEMY HEALTH BECAUSE I MADE IT WITHIN 2 MINUTES)
 
-   public int health = 3;
+    [Tooltip("Material to switch to during the flash.")]
+    [SerializeField]
+    private Material flashMaterial;
 
+    [Tooltip("Duration of the flash.")]
+    [SerializeField] 
+    private float duration;
+
+    public int health = 3;
     public bool Hittable = true;
     public float HitCooldown = 0.8f;
+    private SpriteRenderer spriteRenderer;
+    private Material originalMaterial;
+    private Coroutine flashRoutine;
 
     [SerializeField]
     private Collider thisCollider;
+
+
+    private void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        originalMaterial = spriteRenderer.material;
+    }
 
     private void Update()
     {
@@ -39,6 +56,7 @@ public class QuickTestScript : MonoBehaviour
             if (Hittable)
             {
                 health--;
+                StartCoroutine(FlashRoutine());
                 Hittable = false;
                 thisCollider.enabled = false;
                 HitCooldown = 0.8f;
@@ -49,5 +67,20 @@ public class QuickTestScript : MonoBehaviour
                 gameObject.SetActive(false);    
             }
         }
+    }
+    public void Flash()
+    {
+        if (flashRoutine != null)
+        {
+            StopCoroutine(flashRoutine);
+        }
+        flashRoutine = StartCoroutine(FlashRoutine());
+    }
+    private IEnumerator FlashRoutine()
+    {
+        spriteRenderer.material = flashMaterial;
+        yield return new WaitForSeconds(duration);
+        spriteRenderer.material = originalMaterial;
+        flashRoutine = null;
     }
 }
